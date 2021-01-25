19dedc1cec1397a3fadee2c8bb1f09b2b8716207
Zyad Osseyran <zyad.osseyran@ingrammicro.com>
2021-01-25 18:04:33 +0100

    mail

--- src/main/java/com/Swipeyourjob/Rest_api/dataLayer/DataAccessObjects/DaoImpl/MailDaoImpl.java
+++ src/main/java/com/Swipeyourjob/Rest_api/dataLayer/DataAccessObjects/DaoImpl/MailDaoImpl.java
@@ -22,7 +22,7 @@ public class MailDaoImpl extends BaseDaoSMTP {
             msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
             msg.addHeader("format", "flowed");
             msg.addHeader("Content-Transfer-Encoding", "8bit");
-            msg.setFrom(new InternetAddress("swipeyourjob@gmail.com", "Swipeyourjob"));
+            msg.setFrom(new InternetAddress("developer@gmail.com", "Swipeyourjob"));
             msg.setReplyTo(InternetAddress.parse("swipeyourjob@gmail.com", false));
             msg.setSubject(subject, "UTF-8");
             msg.setText(body, "UTF-8");
